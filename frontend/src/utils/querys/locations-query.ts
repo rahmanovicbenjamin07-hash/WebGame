ee7373f3d43f57d772461695f0cf319d307693a2
rahmanovicbenjamin07-hash <rahmanovicbenjamin07@gmail.com>
2026-04-18 14:23:39 +0200

    User Avatar Query

--- frontend/src/utils/querys/locations-query.ts
+++ frontend/src/utils/querys/locations-query.ts
@@ -3,6 +3,11 @@ interface NewUpload {
     imageUrl:string
 }
 
+interface LocationsList {
+    pageParam:number,
+    limit:number,
+}
+
 export const loadUploadSignedOut = async () => {
     const res = await fetch("http://localhost:3001/location/new/signed-out");
             
@@ -18,4 +23,10 @@ export const fetchLocations = async (limit:number) => {
     if (!res.ok) throw new Error("Failed to fetch new uploads");
     const data: NewUpload[] = await res.json();
     return data;
+}
+
+export const fetchLocationsList = async ({ pageParam = 0, limit=9 }: LocationsList) => {
+    const res = await fetch(`http://localhost:3001/location/new?offset=${pageParam}&limit=${limit}`)
+    if (!res.ok) throw new Error("Failed to fetch locations");
+    return res.json() as Promise<NewUpload[]>;
 }
---
+++ frontend/src/utils/querys/user-query.ts
@@ -0,0 +1,13 @@
+export const fetchUserAvatar = async (userId: number)=>{
+        try {
+            const res = await fetch(`http://localhost:3001/user/${userId}`)
+            if(!res.ok) {
+                throw new Error("Failed to get the avatar");
+            }
+
+            const data = await res.json();
+            return data;            
+        } catch (error) {
+            console.log(error);
+        }
+    }
