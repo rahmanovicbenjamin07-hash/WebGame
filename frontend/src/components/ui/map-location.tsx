855c6f2fefcb0a390646595a3415b97c8be8a421
rahmanovicbenjamin07-hash <rahmanovicbenjamin07@gmail.com>
2026-04-27 13:34:06 +0200

    Fix: Marker delay problem fixed

--- frontend/src/components/ui/map-location.tsx
+++ frontend/src/components/ui/map-location.tsx
@@ -15,9 +15,10 @@ const LocationMap = ({ lat, lng, onLocationSelect, className = "h-63.75" }: Loca
     <MapContainer center={[44.6131, 17.9867]} zoomControl={false} zoom={7} className={`w-full rounded-[19px] z-0 ${className}`}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <LocationPicker onLocationSelect={async (lat, lng) => {
-        const name = await getLocationName(lat, lng);
+        onLocationSelect(lat, lng, "");
+        const name = await getLocationName(lat, lng); 
         onLocationSelect(lat, lng, name);
-      }} />
+    }} />
       {lat !== 0 && lng !== 0 && <Marker position={[lat, lng]} icon={defaultIcon} />}
     </MapContainer>
   );
