1db0b45b2837cbf77747245d0dd6c53ac2cfcd58
rahmanovicbenjamin07-hash <rahmanovicbenjamin07@gmail.com>
2026-05-04 15:27:04 +0200

    refactor: Same return layout for every function

--- server/src/routes/services/guess-service.ts
+++ server/src/routes/services/guess-service.ts
@@ -3,13 +3,12 @@ import { guessesTable, locationsTable, usersTable } from '../../db/schema.js';
 import { eq,asc  } from "drizzle-orm";
 
 export const createGuess = async  (params: {userId: number;locationId: number;guessedLat: number;guessedLng: number;missMeters: number;}) =>  {
-
     const [guess] = await db.insert(guessesTable).values(params).returning();
     return guess;
 }
 
 export const getBestGuesses = async (userId: number) => {
-    return await db.select({
+    const bestGuesses = await db.select({
             id: guessesTable.id,
             missMeters: guessesTable.missMeters,
             imageUrl: locationsTable.locationImage,
@@ -19,4 +18,6 @@ export const getBestGuesses = async (userId: number) => {
         .where(eq(usersTable.id,Number(userId)))
         .orderBy(asc(guessesTable.missMeters))
         .limit(3);
+    
+    return bestGuesses;
 }
